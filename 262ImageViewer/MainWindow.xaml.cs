04f8cd5be653de3a73c50846c27ef568888056b4
7imbrook <timbrook480@gmail.com>
2014-02-12 19:22:02 -0500

    Look loading images

--- 262ImageViewer/MainWindow.xaml.cs
+++ 262ImageViewer/MainWindow.xaml.cs
@@ -23,6 +23,19 @@ namespace _262ImageViewer
         public MainWindow()
         {
             InitializeComponent();
+            Image img = this.testImage;
+            Study studyTest = new Study();
+
+            var basePath = "C:\\Users\\michael\\Desktop\\head_mri";
+            Uri link = new Uri(basePath + "\\mri_head17.JPG");
+
+            BitmapImage src = new BitmapImage();
+            src.BeginInit();
+            src.UriSource = link;
+            src.EndInit();
+
+            img.Source = src;
+
         }
     }
 }
