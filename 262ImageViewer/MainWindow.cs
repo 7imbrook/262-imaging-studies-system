d4f28b0beb4a43266cbf2b1faf4838200bfabc0c
7imbrook <timbrook480@gmail.com>
2014-02-24 13:09:37 -0500

    Readded MainWindow.cs

---
+++ 262ImageViewer/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _262ImageViewer
+{
+    public partial class MainWindow
+    {
+
+        public void createNewStudy()
+        {
+            // Create a new study
+        }
+
+
+    }
+}
