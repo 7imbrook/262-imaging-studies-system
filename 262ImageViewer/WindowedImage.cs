35707f95ac8fa9bf3bf2cb7babd2dc21d9b1d857
nisetto <dmy4874@g.rit.edu>
2014-04-09 22:55:01 -0400

    Created WindowingView and WindowedImage. WindowingView/operation logic implemented

---
+++ 262ImageViewer/WindowedImage.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace _262ImageViewer
+{
+    class WindowedImage
+    {
+
+        private Bitmap processImage(int high, int low, Bitmap to_be_processed)
+        {
+            var image_height = to_be_processed.Height;
+            var image_width = to_be_processed.Width;
+            Bitmap processed_image = new Bitmap(image_width, image_height);
+
+            for (int i = 0; i <= image_height; i++)
+            {
+                for (int j = 0; j <= image_width; j++)
+                {
+                    Color pixel = to_be_processed.GetPixel(i, j);
+                    if (pixel.GetBrightness() < low)
+                    {
+                        Color new_pixel = Color.Black;
+                        processed_image.SetPixel(i, j, new_pixel);
+                    }
+                    else if(pixel.GetBrightness() > high)
+                    {
+                        Color new_pixel = Color.White;
+                        processed_image.SetPixel(i, j, new_pixel);
+                    }
+                    else
+                    {
+                        float sf = equate_scale_factor(high, low, pixel);
+                        int color_value = (int)(sf * 255);
+                        Color new_pixel = Color.FromArgb(color_value, color_value, color_value);
+                        processed_image.SetPixel(i, j, pixel);
+                    }
+
+                }
+            }
+
+            return processed_image;
+
+        }
+
+        private float equate_scale_factor(int high, int low, Color pixel)
+        {
+            float output = ((pixel.GetBrightness() - low)*high) / (high - low);
+            return output;
+        }
+
+    }
+}
---
+++ 262ImageViewer/WindowingView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace _262ImageViewer
+{
+    /// <summary>
+    /// Interaction logic for WindowingView.xaml
+    /// </summary>
+    public partial class WindowingView : Page
+    {
+        public WindowingView()
+        {
+            InitializeComponent();
+            //index = i;
+            //Get image from WindowedImage
+            //display_image() or display_four()
+        }
+        /**
+        private Bitmap getProcessedImage(int high, int low)
+        {
+            
+            return WindowedImage.processImage(high, low, imge);
+        }
+
+        private void display_image(Bitmap image) //Take in processed img
+        {
+            image_display.Children.Clear();
+            System.Windows.Controls.Image i = new System.Windows.Controls.Image();
+            i.Source = image;
+
+            
+        }
+
+        private void display_four(Bitmap image) //Take in processed imgs
+        {
+            image_display.Children.Clear();
+        }
+        **/
+    }
+}
